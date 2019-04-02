e82d0684a7ac82e1aa5e95b58bf624facefc484e
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-04-02 16:04:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,45 +1,45 @@
-class Song 
+class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
   @@artists = []
-  
+
   def initialize(name, artist, genre)
-    @name = name 
-    @artist = artist 
-    @genre = genre 
-    @@count += 1 
-    @@genres << genre 
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
     @@artists << artist
-  end 
-  
-  def self.count 
-    @@count 
-  end 
-  
-  def self.artists 
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
     @@artists.uniq
-  end 
-  
-  def self.genres 
+  end
+
+  def self.genres
     @@genres.uniq
-  end 
-  
+  end
+
   def self.genre_count
     genre_hash = {}
     @@genres.each do |genre|
-      genre_hash[genre] ||= 0 
-      genre_hash[genre] += 1 
-    end 
+      genre_hash[genre] ||= 0
+      genre_hash[genre] += 1
+    end
     genre_hash
   end
-  
-  def self.artist_count 
+
+  def self.artist_count
     artist_hash = {}
     @@artists.each do |artist|
-      artist_hash[artist] ||=0 
-      artist_hash[artist] += 1 
-    end 
+      artist_hash[artist] ||=0
+      artist_hash[artist] += 1
+    end
     artist_hash
   end
 end 
